54fe89166c3b2292bb3e99bf49a97d483fe97204
Damon <dong765@qq.com>
2021-01-18 10:32:56 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "HDQNTool",
-            dependencies: ["Vapor"]),
-        .testTarget(
-            name: "HDQNToolTests",
-            dependencies: ["HDQNTool"]),
+        .target(name: "HDQNTool", dependencies: [
+            .product(name: "Vapor", package: "vapor")
+        ]),
+        .testTarget(name: "HDQNToolTests", dependencies: [
+            .target(name: "App")
+        ]),
     ]
 )
